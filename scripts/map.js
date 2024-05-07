fcfa2d78d3c964ef8aca1f5acb7a2fe523641968
itsfinedd <159060442+itsfinedd@users.noreply.github.com>
2024-05-07 12:04:41 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -23,7 +23,7 @@ function initMap() {
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: "#FF0000",
-    fillOpacity: 0.35,
+    fillOpacity: 0,
     });
 
   arequipaPolygon.setMap(map);
